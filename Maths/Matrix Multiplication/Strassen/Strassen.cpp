bdddd7c1e4993fb33abf0af461c8c7c27e169dbf
Deepak Gupta <deepakgupta1313@gmail.com>
2014-05-23 23:21:28 +0530

    Strassen

--- Maths/Matrix Multiplication/Strassen/Strassen.cpp
+++ Maths/Matrix Multiplication/Strassen/Strassen.cpp
@@ -6,6 +6,7 @@
 #include <complex>
 #include <iostream>
 #include <valarray>
+#include<cstring>
 
 using namespace std;
 
@@ -24,6 +25,14 @@ struct matrix
     {
         dimension=d;
         data=new int[dimension*dimension];
+        //memset(data,0,sizeof(data));
+
+        int i;
+
+        for(i=0;i<dimension*dimension;++i)
+        {
+            data[i]=0;
+        }
     }
 
     matrix(int *mat,int d)
@@ -160,6 +169,146 @@ void printMatrix(matrix mat)
     printf("\n");
 }
 
+void copy_mat(matrix *to,matrix *from,int x1,int y1)
+{
+    int dim=to->getDimension();
+    int i,j;
+
+    for(i=0;i<dim;++i)
+    {
+        for(j=0;j<dim;++j)
+        {
+            to->sett(i,j,from->get(x1+i,y1+j));
+        }
+    }
+}
+
+void add_sub(matrix *to,matrix *a,matrix *b,int fl)
+{
+    int dim=to->getDimension();
+    int i,j;
+
+    for(i=0;i<dim;++i)
+    {
+        for(j=0;j<dim;++j)
+        {
+            to->sett(i,j,a->get(i,j)+fl*(b->get(i,j)));
+        }
+    }
+}
+
+void add_sub2(matrix *to,matrix *from,int x1,int y1,int fl)
+{
+    int dim=from->getDimension();
+    int i,j;
+    int summ;
+
+    for(i=0;i<dim;++i)
+    {
+        for(j=0;j<dim;++j)
+        {
+            summ=fl*from->get(i,j);
+            summ+=to->get(x1*dim+i,y1*dim+j);
+
+            to->sett(x1*dim+i,y1*dim+j,summ);
+        }
+    }
+}
+
+
+void strassen(matrix *a,matrix *b,matrix *c)
+{
+    int x1=0,y1=0,x2=0,y2=0;
+
+    int dim=a->getDimension();
+
+    if(dim==1)
+    {
+        c->sett(0,0,(a->get(0,0))*(b->get(0,0)));
+        return;
+    }
+
+    matrix *s1=new matrix(dim/2);
+    matrix *s2=new matrix(dim/2);
+    matrix *s3=new matrix(dim/2);
+    matrix *s4=new matrix(dim/2);
+    matrix *s5=new matrix(dim/2);
+    matrix *s6=new matrix(dim/2);
+    matrix *s7=new matrix(dim/2);
+    matrix *s8=new matrix(dim/2);
+    matrix *s9=new matrix(dim/2);
+    matrix *s10=new matrix(dim/2);
+
+    matrix *a11=new matrix(dim/2);
+    matrix *a12=new matrix(dim/2);
+    matrix *a21=new matrix(dim/2);
+    matrix *a22=new matrix(dim/2);
+
+    matrix *b11=new matrix(dim/2);
+    matrix *b12=new matrix(dim/2);
+    matrix *b21=new matrix(dim/2);
+    matrix *b22=new matrix(dim/2);
+
+    matrix *p1=new matrix(dim/2);
+    matrix *p2=new matrix(dim/2);
+    matrix *p3=new matrix(dim/2);
+    matrix *p4=new matrix(dim/2);
+    matrix *p5=new matrix(dim/2);
+    matrix *p6=new matrix(dim/2);
+    matrix *p7=new matrix(dim/2);
+
+
+    copy_mat(a11,a,x1,y1);
+    copy_mat(a12,a,x1,y1+dim/2);
+    copy_mat(a21,a,x1+dim/2,y1);
+    copy_mat(a22,a,x1+dim/2,y1+dim/2);
+
+
+    copy_mat(b11,b,x2,y2);
+    copy_mat(b12,b,x2,y2+dim/2);
+    copy_mat(b21,b,x2+dim/2,y2);
+    copy_mat(b22,b,x2+dim/2,y2+dim/2);
+
+
+    add_sub(s1,b12,b22,-1);
+    add_sub(s2,a11,a12,1);
+    add_sub(s3,a21,a22,1);
+    add_sub(s4,b21,b11,-1);
+    add_sub(s5,a11,a22,1);
+    add_sub(s6,b11,b22,1);
+    add_sub(s7,a12,a22,-1);
+    add_sub(s8,b21,b22,1);
+    add_sub(s9,a11,a21,-1);
+    add_sub(s10,b11,b12,1);
+
+    strassen(a11,s1,p1);
+    strassen(s2,b22,p2);
+    strassen(s3,b11,p3);
+    strassen(a22,s4,p4);
+    strassen(s5,s6,p5);
+    strassen(s7,s8,p6);
+    strassen(s9,s10,p7);
+
+    add_sub2(c,p5,0,0,1);
+    add_sub2(c,p4,0,0,1);
+    add_sub2(c,p2,0,0,-1);
+    add_sub2(c,p6,0,0,1);
+
+    add_sub2(c,p1,0,1,1);
+    add_sub2(c,p2,0,1,1);
+
+
+    add_sub2(c,p3,1,0,1);
+    add_sub2(c,p4,1,0,1);
+
+
+    add_sub2(c,p5,1,1,1);
+    add_sub2(c,p1,1,1,1);
+    add_sub2(c,p3,1,1,-1);
+    add_sub2(c,p7,1,1,-1);
+
+}
+
 
 int main()
 {
@@ -171,6 +320,7 @@ int main()
 
     matrix *a=new matrix(dim);
     matrix *b=new matrix(dim);
+
 
     for(i=0;i<dim;++i)
     {
@@ -202,7 +352,13 @@ int main()
 
     cout<<*a<<*b;
 
+
     matrix *c=new matrix(dim);
+    cout<<*c;
+
+    strassen(a,b,c);
+
+    cout<<*c;
 
 	return 0;
 }
